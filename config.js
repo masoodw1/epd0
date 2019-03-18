92bc6ce60d09be4c0c4b8e6e3b757fafd0359430
Masood Ahmed Windows <masoodw1@gmail.com>
2019-03-18 19:16:00 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,6 @@
+var secret_key = "3azy_m4st3r";
+var map_api_key="AIzaSyA_4vP2Sdy_oK_gN2LGp-xItqDM5Xrg2ZA";
+var server_domain = 'http://35.162.61.111/easypik';
+//var server_domain = 'http://192.168.1.31/shajeer/cmc_website/Source';
+//var server_domain = 'http://192.168.138.31/lidiya/callmycab';
+var wordpress = false;
